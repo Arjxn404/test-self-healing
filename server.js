6b0325874e667b3283d607a10b16b3b553a4ee00
Arjxn404 <arjunvasanthakumar2005@gmail.com>
2026-03-17 22:03:26 +0530

    Adding server.js with missing dependency

---
+++ server.js
@@ -0,0 +1,9 @@
+﻿const express = require('express');
+const app = express();
+
+// This will fail because express isn't in package.json
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(3000);
